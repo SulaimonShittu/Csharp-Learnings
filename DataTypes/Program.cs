8656f24ea26fe348c3e2d8c06494543c27ec935e
SulaimonShittu <71654123+SulaimonShittu@users.noreply.github.com>
2024-09-15 12:13:40 +0100

    String lesson

---
+++ DataTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Strings
+string myString;
+myString = "Hello, World";
+string MyString = "Hello, Nigeria";
+
+Console.WriteLine(myString);
+Console.WriteLine(MyString);
+MyString = "Hello, I am";
+
+string name = Console.ReadLine();
+Console.WriteLine(MyString + " " + name);
+
+Console.WriteLine(name.Length);
+
+
