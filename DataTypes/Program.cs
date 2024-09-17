819a1fba6b3be95cb16f97ab465f916d46d0d585
SulaimonShittu <71654123+SulaimonShittu@users.noreply.github.com>
2024-09-17 22:40:01 +0100

    Boolean

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -41,3 +41,33 @@ double floatDivide = 9.9d / 3.5d;
 double floatMultiply = 3.4d * 3.2f;
 
 Console.WriteLine(@$"Addition : {floatSum}, Subtraction : {floatMinus}, Division : {floatDivide}, Multiplication : {floatMultiply}");
+
+//Boolean
+
+bool myTrue;
+myTrue = true;
+bool myFalse = false;
+
+
+bool taf = true && false;
+bool tat = true && true;
+bool faf = false && false;
+
+bool tof = true || false;
+bool tot = true || true;
+bool fof = false || false;
+
+bool notTrue = !(!(false));
+bool notFalse = !false;
+
+
+Console.WriteLine($"True and False : {taf}");
+Console.WriteLine($"True and True : {tat}");
+Console.WriteLine($"False and False : {faf}");
+Console.WriteLine($"True or False : {tof}");
+Console.WriteLine($"True or True : {tot}");
+Console.WriteLine($"False or False : {fof}");
+Console.WriteLine($"Not True : {notTrue}");
+Console.WriteLine($"Not false : {notFalse}");
+
+
